0a4770d621cc74788febe2bc9a24953be56d1d44
f_PLT <pelletier.f@gmail.com>
2022-03-19 14:40:54 -0400

    Rename shapefile_layer constant for more clarity

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ GEOSERVER_URL = "http://0.0.0.0:9876/geoserver"
 AUTH = ("admin", "myawesomegeoserver")
 DATA_BASE_PATH = "/data"
 SHAPEFILE_DATA_PATH = f"{DATA_BASE_PATH}/shapefile"
-SHAPEFILE_TEST_DATA = "test_data"
+SHAPEFILE_LAYER = "test_data"
 
 
 @pytest.fixture(name="geoserver")
--- tests/geoserver_requests/test_datastore.py
+++ tests/geoserver_requests/test_datastore.py
@@ -8,20 +8,19 @@ from pygeoserv.geoserver_requests.workspace import (
     create_workspace_request,
     remove_workspace_request,
 )
-from tests.conftest import AUTH, GEOSERVER_URL, SHAPEFILE_DATA_PATH, SHAPEFILE_TEST_DATA
+from tests.conftest import AUTH, GEOSERVER_URL, SHAPEFILE_DATA_PATH, SHAPEFILE_LAYER
 
 
 class TestDatastoreRequests:
 
     workspace_dict = {
-        "create_shapefile_store": "test-create-shapefile",
-        "configure_shapefile_store": "test-configure-shapefile",
-        "publish_shapefile_layer": "test-publish-shapefile",
-        "remove_shapefile_layer": "test-remove-shapefile",
+        "create_shapefile_store": "test-create-shapefile-request",
+        "configure_shapefile_store": "test-configure-shapefile-request",
+        "publish_shapefile_layer": "test-publish-shapefile-request",
+        "remove_shapefile_layer": "test-remove-shapefile-request",
     }
 
     datastore_name = "shapefile_datastore"
-    data_path = "/data"
 
     def teardown_class(self):
         for _, ws in self.workspace_dict.items():
@@ -57,7 +56,7 @@ class TestDatastoreRequests:
             AUTH,
             self.workspace_dict["configure_shapefile_store"],
             self.datastore_name,
-            self.data_path,
+            SHAPEFILE_DATA_PATH,
         )
         assert response.status_code == 200
 
@@ -83,32 +82,39 @@ class TestDatastoreRequests:
             AUTH,
             self.workspace_dict["publish_shapefile_layer"],
             self.datastore_name,
-            SHAPEFILE_TEST_DATA,
+            SHAPEFILE_LAYER,
         )
         assert response.status_code == 201
 
     def test_remove_shapefile_layer(self):
         create_workspace_request(
-            GEOSERVER_URL, AUTH, self.workspace_dict["publish_shapefile_layer"]
+            GEOSERVER_URL, AUTH, self.workspace_dict["remove_shapefile_layer"]
         )
         create_shapefile_store_request(
             GEOSERVER_URL,
             AUTH,
-            self.workspace_dict["publish_shapefile_layer"],
+            self.workspace_dict["remove_shapefile_layer"],
             self.datastore_name,
         )
         configure_shapefile_store_request(
             GEOSERVER_URL,
             AUTH,
-            self.workspace_dict["publish_shapefile_layer"],
+            self.workspace_dict["remove_shapefile_layer"],
             self.datastore_name,
             SHAPEFILE_DATA_PATH,
+        )
+        publish_shapefile_layer_request(
+            GEOSERVER_URL,
+            AUTH,
+            self.workspace_dict["remove_shapefile_layer"],
+            self.datastore_name,
+            SHAPEFILE_LAYER,
         )
         response = remove_shapefile_layer_request(
             GEOSERVER_URL,
             AUTH,
-            self.workspace_dict["publish_shapefile_layer"],
+            self.workspace_dict["remove_shapefile_layer"],
             self.datastore_name,
-            SHAPEFILE_TEST_DATA,
+            SHAPEFILE_LAYER,
         )
         assert response.status_code == 200
